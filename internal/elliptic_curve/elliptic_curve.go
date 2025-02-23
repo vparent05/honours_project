7f203c1fcc2bb9b583788fb41cdbb004eb51440a
jukuly <vpare070@uottawa.ca>
2025-02-23 12:54:14 -0500

    made it work with large numbers

--- internal/elliptic_curve/elliptic_curve.go
+++ internal/elliptic_curve/elliptic_curve.go
@@ -1,38 +1,38 @@
-package ellipticcurve
+package elliptic_curve
 
 import (
 	"fmt"
-
-	"github.com/jukuly/honours_project/internal/utils"
+	"math/big"
 )
 
 // Use nil to represent the point at infinity
-type point struct {
-	X     int
-	Y     int
+type Point struct {
+	X     *big.Int
+	Y     *big.Int
 	curve *EllipticCurve
 }
 
 // y^2 = x^3 + ax + b (mod p)
 type EllipticCurve struct {
-	A int
-	B int
-	P int
+	A *big.Int
+	B *big.Int
+	P *big.Int
 }
 
-func (pt *point) Equals(other *point) bool {
-	return pt.X == other.X && pt.Y == other.Y && pt.curve == other.curve
+func (pt *Point) Equals(other *Point) bool {
+	return pt.X.Cmp(other.X) == 0 && pt.Y.Cmp(other.Y) == 0 && pt.curve == other.curve
 }
 
-func (ec *EllipticCurve) Point(x, y int) (*point, error) {
-	if y*y != x*x*x+ec.A*x+ec.B {
+func (ec *EllipticCurve) Point(x, y *big.Int) (*Point, error) {
+	var z big.Int
+	if z.Exp(y, big.NewInt(2), ec.P) != z.Add(z.Add(z.Exp(x, big.NewInt(3), ec.P), z.Mul(ec.A, x)), ec.B) {
 		return nil, fmt.Errorf("point (%d, %d) is not on the curve", x, y)
 	}
 
-	return &point{x % ec.P, y % ec.P, ec}, nil
+	return &Point{x.Mod(x, ec.P), y.Mod(y, ec.P), ec}, nil
 }
 
-func (pt *point) Add(other *point) (*point, error) {
+func (pt *Point) Add(other *Point) (*Point, error) {
 	if pt.curve != other.curve {
 		return nil, fmt.Errorf("points are not on the same curve")
 	}
@@ -43,43 +43,55 @@ func (pt *point) Add(other *point) (*point, error) {
 		return pt, nil
 	}
 
-	if pt.X == other.X && (pt.Y != other.Y || pt.Y == 0) {
+	if pt.X.Cmp(other.X) == 0 && (pt.Y.Cmp(other.Y) != 0 || pt.Y.Cmp(big.NewInt(0)) == 0) {
 		return nil, nil
 	}
 
-	var m int
+	var m *big.Int
+	var temp *big.Int
 	if pt.Equals(other) {
-		m = (3*pt.X*pt.X + pt.curve.A) * utils.Inverse(2*pt.Y, pt.curve.P) % pt.curve.P
+		m = temp.Mod(
+			temp.Mul(
+				temp.Mul(
+					big.NewInt(3),
+					temp.Exp(pt.X, big.NewInt(2), pt.curve.P)),
+				temp.ModInverse(temp.Mul(big.NewInt(2), pt.Y), pt.curve.P)),
+			pt.curve.P)
 	} else {
-		m = (other.Y - pt.Y) * utils.Inverse(other.X-pt.X, pt.curve.P) % pt.curve.P
+		m = temp.Mod(
+			temp.Mul(
+				temp.Sub(other.Y, pt.Y),
+				temp.ModInverse(temp.Sub(other.X, pt.X), pt.curve.P)),
+			pt.curve.P)
 	}
 
-	x := m*m - pt.X - other.X
-	y := m*(pt.X-x) - pt.Y
+	x := temp.Sub(temp.Exp(m, big.NewInt(2), pt.curve.P), temp.Add(pt.X, other.X))
+	y := temp.Sub(temp.Mul(m, temp.Sub(pt.X, x)), pt.Y)
 
 	return pt.curve.Point(x, y)
 }
 
-func (pt *point) Negate() *point {
-	res, _ := pt.curve.Point(pt.X, -pt.Y)
+func (pt *Point) Negate() *Point {
+	res, _ := pt.curve.Point(pt.X, pt.Y.Neg(pt.Y))
 	return res
 }
 
-func (pt *point) Multiply(n int) *point {
-	var Q *point = nil
+func (pt *Point) Multiply(n *big.Int) *Point {
+	var Q *Point = nil
 
 	R := pt
-	if n < 0 {
-		R = R.Negate()
-		n = -n
+	if n.Cmp(big.NewInt(0)) == -1 {
+		R.Negate()
+		n.Neg(n)
 	}
 
-	for n > 0 {
-		if n%2 == 1 {
+	var temp *big.Int
+	for n.Cmp(big.NewInt(0)) == 1 {
+		if temp.Mod(n, big.NewInt(2)).Int64() == 1 {
 			Q, _ = Q.Add(R)
 		}
 		R, _ = R.Add(R)
-		n /= 2
+		n.Div(n, big.NewInt(2))
 	}
 
 	return Q
