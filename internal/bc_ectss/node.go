c5cae3236be0ab2926e9ba5e4ada9884a77833aa
jukuly <vpare070@uottawa.ca>
2025-03-06 12:09:20 -0500

    removed singleton pattern from system structure

--- internal/bc_ectss/node.go
+++ internal/bc_ectss/node.go
@@ -9,11 +9,12 @@ import (
 )
 
 type node struct {
-	id  *big.Int
-	a   []*big.Int
-	eta []*elliptic_curve.Point
-	sk  *big.Int
-	pk  *elliptic_curve.Point
+	id     *big.Int
+	a      []*big.Int
+	eta    []*elliptic_curve.Point
+	sk     *big.Int
+	pk     *elliptic_curve.Point
+	system *System
 }
 
 type Signature struct {
@@ -22,20 +23,22 @@ type Signature struct {
 	Beta  *big.Int
 }
 
-func NewNode(id int) *node {
+func (sys *System) NewNode(id int) *node {
 	node := node{}
 	node.id = big.NewInt(int64(id))
-	node.a = make([]*big.Int, GetSystem().Threshold)
+	node.a = make([]*big.Int, sys.Threshold)
 
-	for i := 0; i < GetSystem().Threshold; i++ {
-		node.a[i], _ = rand.Int(rand.Reader, GetSystem().Order.Sub(GetSystem().Order, big.NewInt(1)))
+	for i := 0; i < sys.Threshold; i++ {
+		node.a[i], _ = rand.Int(rand.Reader, sys.Order.Sub(sys.Order, big.NewInt(1)))
 		node.a[i].Add(node.a[i], big.NewInt(1))
 	}
 
-	node.eta = make([]*elliptic_curve.Point, GetSystem().Threshold)
-	for i := 0; i < GetSystem().Threshold; i++ {
-		node.eta[i] = GetSystem().G.Multiply(node.a[i])
+	node.eta = make([]*elliptic_curve.Point, sys.Threshold)
+	for i := 0; i < sys.Threshold; i++ {
+		node.eta[i] = sys.G.Multiply(node.a[i])
 	}
+
+	node.system = sys
 
 	return &node
 }
@@ -44,27 +47,27 @@ func (n *node) f(x *big.Int) *big.Int {
 	res := big.NewInt(0)
 	var temp *big.Int
 	for i := 0; i < len(n.a); i++ {
-		res.Add(res, temp.Mul(n.a[i], temp.Exp(x, big.NewInt(int64(i)), GetSystem().Order)))
+		res.Add(res, temp.Mul(n.a[i], temp.Exp(x, big.NewInt(int64(i)), n.system.Order)))
 	}
-	return res.Mod(res, GetSystem().Order)
+	return res.Mod(res, n.system.Order)
 }
 
 func (n *node) Chi() *big.Int {
 	chi := big.NewInt(1)
 	var temp *big.Int
-	for i := 0; i < GetSystem().Threshold; i++ {
-		id_j := GetSystem().Users[i].id
+	for i := 0; i < n.system.Threshold; i++ {
+		id_j := n.system.Users[i].id
 		if id_j == n.id {
 			continue
 		}
 		chi.Mul(chi, temp.Neg(id_j))
-		chi.Mul(chi, temp.ModInverse(temp.Sub(n.id, id_j), GetSystem().Order))
+		chi.Mul(chi, temp.ModInverse(temp.Sub(n.id, id_j), n.system.Order))
 	}
-	return chi.Mod(chi, GetSystem().Order)
+	return chi.Mod(chi, n.system.Order)
 }
 
 func (n *node) SetKeys() error {
-	for _, n_i := range GetSystem().Users {
+	for _, n_i := range n.system.Users {
 		if n_i.id == n.id {
 			continue
 		}
@@ -74,48 +77,52 @@ func (n *node) SetKeys() error {
 	}
 
 	n.sk = big.NewInt(0)
-	for _, n_i := range GetSystem().Users {
+	for _, n_i := range n.system.Users {
 		n.sk.Add(n.sk, n_i.f(n.id))
 	}
 
-	n.pk = GetSystem().G.Multiply(n.sk)
+	n.pk = n.system.G.Multiply(n.sk)
 	return nil
 }
 
 // verify the secret share from node n_j to node n_i
 func (n_i *node) verifySecretShare(n_j *node) bool {
-	rhs := make([]*elliptic_curve.Point, GetSystem().Threshold)
+	if n_i.system != n_j.system {
+		return false
+	}
+
+	rhs := make([]*elliptic_curve.Point, n_i.system.Threshold)
 	var temp *big.Int
-	for i := 0; i < GetSystem().Threshold; i++ {
-		n_j.eta[i].Multiply(temp.Exp(n_i.id, big.NewInt(int64(i)), GetSystem().Order))
+	for i := 0; i < n_i.system.Threshold; i++ {
+		n_j.eta[i].Multiply(temp.Exp(n_i.id, big.NewInt(int64(i)), n_i.system.Order))
 	}
 	rhsSum, _ := elliptic_curve.PointSum(rhs)
 
-	return GetSystem().G.Multiply(n_j.f(n_i.id)).Equals(rhsSum)
+	return n_i.system.G.Multiply(n_j.f(n_i.id)).Equals(rhsSum)
 }
 
 func (n *node) GetPartialSignature(message *big.Int) *Signature {
-	k, _ := rand.Int(rand.Reader, GetSystem().Order.Sub(GetSystem().Order, big.NewInt(1)))
+	k, _ := rand.Int(rand.Reader, n.system.Order.Sub(n.system.Order, big.NewInt(1)))
 	k.Add(k, big.NewInt(1))
 
-	e := Hash(message)
+	e := n.system.Hash(message)
 
-	point := GetSystem().G.Multiply(k)
+	point := n.system.G.Multiply(k)
 
-	r := point.X.Mod(point.X, GetSystem().Order)
+	r := point.X.Mod(point.X, n.system.Order)
 	if r.Cmp(big.NewInt(0)) == 0 {
 		r = big.NewInt(1)
 	}
 
-	beta, _ := rand.Int(rand.Reader, GetSystem().Order.Sub(GetSystem().Order, big.NewInt(1)))
+	beta, _ := rand.Int(rand.Reader, n.system.Order.Sub(n.system.Order, big.NewInt(1)))
 	beta.Add(beta, big.NewInt(1))
 
 	var temp *big.Int
-	alpha := temp.Mul(temp.Sub(k, temp.Mul(beta, message)), temp.ModInverse(r, GetSystem().Order))
-	alpha.Mod(alpha, GetSystem().Order)
+	alpha := temp.Mul(temp.Sub(k, temp.Mul(beta, message)), temp.ModInverse(r, n.system.Order))
+	alpha.Mod(alpha, n.system.Order)
 
 	chi := n.Chi()
-	l := temp.Mod(temp.Add(temp.Mul(alpha, r), temp.Mul(temp.Mul(e, chi), n.sk)), GetSystem().Order)
+	l := temp.Mod(temp.Add(temp.Mul(alpha, r), temp.Mul(temp.Mul(e, chi), n.sk)), n.system.Order)
 
 	return &Signature{point, l, beta}
 }
@@ -123,34 +130,34 @@ func (n *node) GetPartialSignature(message *big.Int) *Signature {
 func (n *node) VerifyPartialSignature(message *big.Int, sig *Signature) bool {
 
 	var temp *big.Int
-	gamma := temp.Mod(temp.Add(sig.L, temp.Mul(sig.Beta, message)), GetSystem().Order)
+	gamma := temp.Mod(temp.Add(sig.L, temp.Mul(sig.Beta, message)), n.system.Order)
 
-	e := Hash(message)
+	e := n.system.Hash(message)
 
 	chi := n.Chi()
-	point, _ := GetSystem().G.Multiply(gamma).Add(n.pk.Multiply(e.Mul(e, chi)).Negate())
+	point, _ := n.system.G.Multiply(gamma).Add(n.pk.Multiply(e.Mul(e, chi)).Negate())
 
 	return point.Equals(sig.Point)
 }
 
-func GetSignature(partialSignatures []*Signature) *Signature {
+func (sys *System) GetSignature(partialSignatures []*Signature) *Signature {
 	l := big.NewInt(0)
 	beta := big.NewInt(0)
 	var point *elliptic_curve.Point
 	for _, sig := range partialSignatures {
-		l.Mod(l.Add(l, sig.L), GetSystem().Order)
-		beta.Mod(beta.Add(beta, sig.Beta), GetSystem().Order)
+		l.Mod(l.Add(l, sig.L), sys.Order)
+		beta.Mod(beta.Add(beta, sig.Beta), sys.Order)
 		point.Add(sig.Point)
 	}
 	return &Signature{point, l, beta}
 }
 
-func VerifySignature(message *big.Int, sig *Signature) bool {
+func (sys *System) VerifySignature(message *big.Int, sig *Signature) bool {
 	var temp *big.Int
-	gamma := temp.Mod(temp.Add(sig.L, temp.Mul(sig.Beta, message)), GetSystem().Order)
+	gamma := temp.Mod(temp.Add(sig.L, temp.Mul(sig.Beta, message)), sys.Order)
 
-	e := Hash(message)
-	point, _ := GetSystem().G.Multiply(gamma).Add(GetSystem().PublicKey.Multiply(e).Negate())
+	e := sys.Hash(message)
+	point, _ := sys.G.Multiply(gamma).Add(sys.PublicKey.Multiply(e).Negate())
 
 	return point.Equals(sig.Point)
 }
--- internal/bc_ectss/system.go
+++ internal/bc_ectss/system.go
@@ -1,7 +1,9 @@
 package bc_ectss
 
 import (
+	"crypto/rand"
 	"crypto/sha256"
+	"errors"
 	"math/big"
 
 	"github.com/jukuly/honours_project/internal/elliptic_curve"
@@ -15,26 +17,13 @@ type System struct {
 	PublicKey *elliptic_curve.Point
 }
 
-var sys *System
-
-func SetSystem(newSys *System) {
-	sys = newSys
-}
-
-func GetSystem() *System {
-	if sys == nil {
-		panic("System not initialized")
-	}
-	return sys
-}
-
-func Hash(plain *big.Int) *big.Int {
+func (sys *System) Hash(plain *big.Int) *big.Int {
 	hash := sha256.New()
 	hash.Write(plain.Bytes())
 
 	var digest *big.Int
 	digest.SetBytes(hash.Sum(nil))
-	return digest.Mod(digest, GetSystem().Order)
+	return digest.Mod(digest, sys.Order)
 }
 
 func (s *System) SetPublicKey() *elliptic_curve.Point {
@@ -45,4 +34,45 @@ func (s *System) SetPublicKey() *elliptic_curve.Point {
 	}
 
 	return s.PublicKey
+}
+
+type Ciphertext struct {
+	c1 *elliptic_curve.Point
+	c2 *big.Int
+	c3 *elliptic_curve.Point
+	c4 *elliptic_curve.Point
+}
+
+func (sys *System) Encrypt(plaintext *elliptic_curve.Point, tag *big.Int) (*Ciphertext, error) {
+	var temp *big.Int
+	alpha, _ := rand.Int(rand.Reader, sys.Order)
+
+	c1, err := sys.PublicKey.Multiply(temp.Mul(alpha, sys.Hash(tag))).Add(plaintext)
+	if err != nil {
+		return nil, errors.New("plaintext is not on the system curve")
+	}
+
+	c2 := temp.Neg(alpha)
+	c3 := sys.G.Multiply(alpha)
+	c4 := sys.G.Multiply(temp.Mul(alpha, sys.Hash(tag)))
+
+	return &Ciphertext{c1, c2, c3, c4}, nil
+}
+
+func Decrypt(ciphertext *Ciphertext, signature *Signature) (*elliptic_curve.Point, error) {
+	dotProduct, err := elliptic_curve.PointSum([]*elliptic_curve.Point{
+		signature.Point.Multiply(ciphertext.c2),
+		ciphertext.c3.Multiply(signature.L),
+		ciphertext.c4.Multiply(signature.Beta),
+	})
+	if err != nil {
+		return nil, errors.New("invalid ciphertext")
+	}
+
+	decrypted, err := ciphertext.c1.Add(dotProduct.Negate())
+	if err != nil {
+		return nil, errors.New("invalid ciphertext")
+	}
+
+	return decrypted, nil
 }
